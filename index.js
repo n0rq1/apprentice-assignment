97aa917fac720cfa007e14627b083ed58a5e30d1
Austin <anorquist@mail.csuchico.edu>
2025-03-10 19:32:03 -0700

    Please work

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.json(response);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
