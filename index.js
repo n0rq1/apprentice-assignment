06c5a009e891933851309b338b742abffaa9e53e
Austin <anorquist@mail.csuchico.edu>
2025-03-10 19:34:52 -0700

    Reverted index.js change and testing dockerfile change

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.json(response);
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
