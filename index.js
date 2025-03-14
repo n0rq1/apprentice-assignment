96e1630265776f5d943294ed284c848f7ff4e481
Austin <anorquist@mail.csuchico.edu>
2025-03-14 00:38:26 -0700

    test

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ app.get('/', (req, res) => {
     const response = {
       message: "My name is Austin Norquist",
       timestamp: Date.now(),
-      testing: "test"
+      testing: "test",
+      new: "value"
     };
     res.json(response);
 });
