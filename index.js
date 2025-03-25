5cae9236323f824c60e4baa6cc68e6218d9448d7
Austin <anorquist@mail.csuchico.edu>
2025-03-25 11:15:30 -0700

    fix: to match with demo

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 const PORT = 80;
 
 app.get('/', (req, res) => {
-    const response = {
-      message: "My name is Austin Norquist",
-      timestamp: Date.now(),
-      Liatrio: "DevOps"
-    };
-    res.json(response);
+  const response = {
+    message: "My name is Austin Norquist",
+    timestamp: Date.now(),
+    Liatrio: "demo!"
+  };
+  res.json(response);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
