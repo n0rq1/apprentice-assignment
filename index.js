95f8a9c7d1a121b946b4a435e32bfebf895dcc30
Austin <anorquist@mail.csuchico.edu>
2025-03-27 11:37:01 -0700

    fix: idk anymore

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 80;
 
-app.set('json spaces', 0);
-
 app.get('/', (req, res) => {
-  let ts = Date.now()
-  const response = {
-    message: 'My name is Austin',
-    timestamp: ts
-  };
-  res.json(response);
+  res.json({message: "My name is Austin", timestamp: Date.now()});
 });
 
 app.listen(PORT, () => {
